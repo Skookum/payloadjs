404c55d856a886d6c22f06f034c95e3f5e45c4a5
Chuck Preslar <chuckpreslar@gmail.com>
2012-11-05 10:35:05 -0500

    Scope server and connections within own domain to prevent crashes on error.
    Signed-off-by: Chuck Preslar <chuckpreslar@gmail.com>

--- payload-server.js
+++ payload-server.js
@@ -2,25 +2,47 @@
 
   var net = require('net')
     , Buffer = require('buffer')
-    , Payload = require('./libs/payload');
+    , domain = require('domain')
+    , Payload = require('./libs/payload')
+    , serverDomain = domain.create();
 
-  var server = net.createServer(function(connection) {
-    console.log('Client connected');
-    connection.on('data', function(data) {
-      var payload = new Payload()
-        , data = JSON.parse(data);
-      console.log('Data received - ', data);
-      if(typeof payload[data.method] !== 'undfined') {
-        payload[data.method](data.location, data.asset_types, data.iterations || 1, function(err, results) {
-          console.log('Sending results');
-          connection.write(JSON.stringify(results));
-        });
-      } else {
-        connection.write(JSON.stringify({ err: 'Invalid operation' }));
-      }
-    });
+  serverDomain.run(function() {
+    var server = net.createServer(function(connection) {
+
+      console.log('Client connected');
+
+      var connectionDomain = domain.create();
+          connectionDomain.add(connection);
+
+      connection.on('data', function(data) {
+        try {
+          var payload = new Payload()
+            , data = JSON.parse(data);
+          console.log('Data received:\n', data);
+          if(typeof payload[data.method] !== 'undfined') {
+            payload[data.method](data.location, data.asset_types, data.iterations || 1, function(err, results) {
+              console.log('Sending results');
+              connection.write(JSON.stringify(results));
+            });
+          } else {
+            connection.write(JSON.stringify({ err: new Error('Invalid operation') }));
+          }
+        } catch(e) {
+          // Handle errors caused by handling connection
+          connection.destroy();
+          connectionDomain.dispose();
+        }
+      });
+
+      // Dispose of domain on connection close
+      connection.on('close', function() { connectionDomain.dispose() });
+
+    }).listen(3333, function() { console.log('Payload server listening on port 3333') });
+
   });
 
-  server.listen(3333, function() { console.log('Payload server listening on port 3333') });
+  serverDomain.on('error', function(err) {
+    console.log('The server encountered an error:\n', err)
+  });
 
 }())
--- test.js
+++ test.js
@@ -6,12 +6,12 @@
   socket.connect(3333);
 
   socket.on('connect', function() {
-    var options = { method: 'ramp', location: 'http://www.chuckpreslar.com', asset_types: ['images', 'css', 'scripts'], iterations: 10 };
+    var options = { method: 'flood', location: 'http://www.chuckpreslar.com', asset_types: ['images', 'css', 'scripts'], iterations: 1 };
     socket.write(JSON.stringify(options));
   });
 
   socket.on('data', function(data) {
-    console.log(JSON.parse(data));
+    console.log(JSON.parse(data)[0].assets.images);
   });
 
 }())
